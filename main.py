2f3c3b4a06bd9b18731449853675cd4d108814ce
Edward Ramirez <edal_ramirez@hotmail.com>
2020-10-18 19:03:44 -0500

    Adding more instructions
    In this commit, I changed the description displayed on screen so the user can provide valid information. For example,
    in line 30 I'm asking the user to put the hour in the correct format.
    
    I also changed the database name and the fields used in the SQL Query in the deleteMeeting method because I had incorrect
    data.
    
    The last change was ask the users for the amount of meetings that they want to add and execute the gettingValues and
    addingToZoomDatabase the times that they want to add different meetings to the Database

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sqlite3
 import time
 import pyautogui
-import getpass
+# import getpass
 import sys
 import os
 import webbrowser
+# from selenium import webdriver
 pyautogui.FAILSAFE = False
 
 
@@ -23,9 +24,11 @@ class MeetingDatabase:
 
         try:
             self.subject = input("Enter the subject: ").strip().title()
-            self.day = input("Enter the class day: ").strip().title()[:3]
-            self.digitalHour = input("Enter the class hour: ").split(':')
-            self.classLength = input("Enter the Class Length: ").strip()
+            self.day = input(
+                "Enter the class day (Example: Tuesday): ").strip().title()[:3]
+            self.digitalHour = input(
+                "Enter the class hour (Ex: 7:30, 15:00): ").split(':')
+            self.classLength = input("Enter the Class Length(Ex: 2): ").strip()
             self.hour = int(self.digitalHour[0])
 
             try:
@@ -68,14 +71,16 @@ class MeetingDatabase:
             subject = input(
                 "Enter the subject that you want to delete: ").strip().title()
             day = input("Enter the class day: ").strip().title()[:3]
-            hour = int(input("Enter the hour: "))
+            digitalHour = input(
+                "Enter the class hour (Ex: 7:30, 15:00): ").split(':')
+            hour = int(digitalHour[0])
             self.cur.execute(
-                f"SELECT Subject FROM Database WHERE Subject='{subject}' AND Day='{day}' AND Hour={hour}")
+                f"SELECT materia FROM ZoomDatabase WHERE materia='{subject}' AND dia='{day}' AND hora={hour}")
             if self.cur.fetchone() is None:
                 print("You have enter bad data. It's not in the database")
             else:
                 self.cur.execute(
-                    f"DELETE FROM Database WHERE Subject='{subject}' AND Day='{day}' AND Hour={hour}")
+                    f"DELETE FROM ZoomDatabase WHERE materia='{subject}' AND dia='{day}' AND hora={hour}")
                 self.conn.commit()
                 print("You have deleted the meeting")
         except ValueError:
@@ -119,10 +124,10 @@ class GoToClass:
 
         # time.sleep(0.5)
         pyautogui.click(coordinates)
-        time.sleep(0.5)
+        time.sleep(1)
         pyautogui.write(id)
         pyautogui.press('enter')
-        time.sleep(1.5)
+        time.sleep(2.5)
         pyautogui.write(password)
         pyautogui.press('enter')
 
@@ -141,8 +146,12 @@ class GoToClass:
 
     def meetEntrySelenium(self, url):
         PATH = os.getcwd() + "\\chromedriver.exe"
-        # driver = webdriver.Chrome(PATH)
-        # driver.get(url)
+        options = webdriver.ChromeOptions()
+        options.add_argument(
+            r"--user-data-dir=C:\\Users\\Casa De Jose\\AppData\\Local\\Google\\Chrome\\User Data")
+        options.add_argument("start-maximized")
+        driver = webdriver.Chrome(PATH, options=options)
+        driver.get(url)
 
 
 def staying():
@@ -168,7 +177,7 @@ def main():
         print("5.Adding Google Meet URL")
         print("6.Go to Meet Class")
         print("7.Exit")
-        print("*"*40)
+        print("*"*50)
         answer = input("What do you want to do?(1-7): ").strip()
 
         if answer == "1":
@@ -176,9 +185,19 @@ def main():
             print("You have made a new table.")
             staying()
         elif answer == "2":
-            zoomDatabase.gettingValues()
-            zoomDatabase.addingToZoomDatabase()
-            print("Meeting added to your database")
+            try:
+                meetings = int(
+                    input("How many meetings do you want to add?(Example: 2): "))
+                n = 0
+                while meetings > n:
+                    zoomDatabase.gettingValues()
+                    zoomDatabase.addingToZoomDatabase()
+                    print("Meeting added to your database")
+                    print("*"*50)
+                    n += 1
+            except ValueError:
+                print("Enter a digit please.")
+
             staying()
         elif answer == "3":
             zoomDatabase.deleteMeeting()
@@ -214,6 +233,15 @@ def main():
             print("Until the next one.")
             zoomDatabase.cur.close()
             break
+        elif answer == "8":
+            day, hour, minute = entry.get_time()
+            meetURL = googleMeet.retrieveGoogleMeet(day, hour, minute)
+            if meetURL is None:
+                print(
+                    "This class doesn't exist. Check the data and remember that you can enter until 10 minutes before class.")
+            else:
+                entry.meetEntrySelenium(meetURL[0])
+            staying()
 
         else:
             print("Invalid. Try again")
@@ -221,18 +249,19 @@ def main():
 
 
 if __name__ == '__main__':
-    ADMIN_PASSWORD = "CONTRASEA."
+    # ADMIN_PASSWORD = "CONTRASEA."
     print("Welcome to your Zoom-Automation Manager")
     print("*"*40)
-    try:
-        while True:
-            getpassword = getpass.getpass("Enter your main Password: ")
-            if getpassword == ADMIN_PASSWORD:
-                main()
-                break
-            else:
-                print("Invalid access.\nTry again or press Ctrl-C to exit")
-                print("*"*40)
+    main()
+    # try:
+    # while True:
+    #     getpassword = getpass.getpass("Enter your main Password: ")
+    #     if getpassword == ADMIN_PASSWORD:
+    #         main()
+    #         break
+    #     else:
+    #         print("Invalid access.\nTry again or press Ctrl-C to exit")
+    #         print("*"*40)
 
-    except KeyboardInterrupt:
-        print("\nUntil the next one")
+    # except KeyboardInterrupt:
+    #     print("\nUntil the next one")
